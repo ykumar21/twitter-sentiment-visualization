7a9781a4eb17570c1b8af25e81bdec739a3befb7
Tandoori Momos <yash20008@gmail.com>
2019-06-05 21:43:00 +0530

    Initialized cookie sessions

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
+// npm modules
+
 const express = require('express');
 const ejs = require('ejs');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 
+// Routes
 const routes = require('./routes.js');
 const authRoutes = require('./routes/auth-routes.js');
 
@@ -26,6 +30,16 @@ mongoose.connect(process.env.MONGODB_URL, {
 }).catch((err) => {
   console.log(err);
 });
+
+// Setup cookies
+app.use(cookieSession({
+  maxAge: 24 * 60 * 60 * 100, // cookie will expire after 1 day
+  key: [process.env.COOKIE_KEY]
+}));
+
+// Initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 const altPort = 8080;
 const server = app.listen(process.env.PORT || altPort, () => {
