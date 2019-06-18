b59540706b6737c008a8df85f769862224c32ad8
Tandoori Momos <yash20008@gmail.com>
2019-06-18 23:16:14 +0530

    Added empty request handler

--- src/public/scripts/map.js
+++ src/public/scripts/map.js
@@ -1,7 +1,34 @@
 const socket = io.connect('http://localhost:8080');
-let status = document.getElementById('status').innerHTML;
+let fire = 0; // Checks if 'final' event has been fired
+function HandleEmptyRequest(target, time) {
+  // Checks if the 'final' event fires after 
+  // a 30 seconds. If the event doesn't fire
+  // means that an empty request has been 
+  // recieved by the server and so the user is
+  // redirected back to home
+
+  setTimeout(function() {
+    if(!fire) {
+      swal({
+        title: 'Connection Timeout!',
+        text: 'Please try searching for the query again!',
+        type: 'error',
+
+      })
+      $('.swal2-confirm').click(function(){
+        window.location.href = '/';
+      });
+      
+    }
+  }, time * 1000);
+}
+
+// Check for empty request after 30s 
+HandleEmptyRequest('final', 30);
+
 socket.on('final', function(data) {
-  status = 'Rendering Map!';
+  fire = 1;
+  document.getElementById('status').innerHTML = 'Back to home';
   let dataTable = []; // 2D Array
   let col = ['State', 'Score'];
 
@@ -30,9 +57,6 @@ function DrawMap(dataTable) {
 
     const opts = {
       backgroundColor: '#ecf0f1',
-      colorAxis: {
-         colors: ['red', 'green']
-      },
       region: 'US',
       displayMode: 'regions',
       resolution: 'provinces',
